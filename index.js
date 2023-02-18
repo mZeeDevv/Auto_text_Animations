d0c4d6328a0c845c46e6c80292f65644ea6065e6
Hamza Shaukat <hamzeekhattak56@gmail.com>
2023-02-18 19:44:32 +0500

    Added JavaScript

---
+++ index.js
@@ -0,0 +1,19 @@
+let text = document.querySelector(".container");
+let jobs = ["Youtuber", "Freelancer" , "Web Developer", "Student"];
+let jobIndex = 0;
+let jobChange = 0;
+
+updateText()
+function updateText(){
+    jobChange++
+text.innerHTML = `<h3>mZee Dev a ${jobs[jobIndex].slice(0, jobChange)} <h3>`;
+if(jobChange == jobs[jobIndex].length){
+    jobIndex++
+    jobChange = 0;
+}
+if(jobs == jobs.length){
+    jobs = 2;
+}
+
+setTimeout(updateText, 400)
+}
